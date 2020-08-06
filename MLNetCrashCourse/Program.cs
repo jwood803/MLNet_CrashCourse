bee03ede13dca956cedb5aab7e0f540ea0c44794
J W <woodjon@gmail.com>
2020-08-06 10:46:33 -0400

    Add project files

---
+++ MLNetCrashCourse/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ML;
+using System;
+using System.Linq;
+
+namespace MLNetCrashCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true,
+                separatorChar: ',');
+
+            var split = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+            var features = split.TrainSet.Schema
+                .Select(col => col.Name)
+                .Where(colName => colName != "Label" && colName != "OceanProximity")
+                .ToArray();
+
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Feature", "Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(split.TrainSet);
+
+            var predictions = model.Transform(split.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            Console.WriteLine($"R^2 - {metrics.RSquared}");
+        }
+    }
+}
